86cbba53f038da2ec1ff1026c430303d0f20db34
Jack <jbdeletewhendone@gmail.com>
2024-08-24 17:41:25 -0400

    ufdev.sh

---
+++ install/linux/ufdev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'export MATLABPATH=~/UFDEV/Project/matlab/latex;~/UFDEV/Project/matlab/MPS/common;~/UFDEV/Project/matlab/MPS;~/UFDEV/Project/matlab/MPS/main;~/UFDEV/Project/matlab/MPS/main_rods' >> ~/.bashrc
+source ~/.bashrc
+echo $MATLABPATHcd ..
