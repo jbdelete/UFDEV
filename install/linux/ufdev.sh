ec972508ef0b3d5ecfdbd347fffcbdb920a6e07d
JBell <jb2ndr@gmail.com>
2024-08-28 11:13:11 -0400

    edited bash.sh

--- install/linux/ufdev.sh
+++ install/linux/ufdev.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-read -p "Please Enter Path:" -r r1
+read -p "Please Enter Path to the directory that contains UFDEV:" -r r1
 parent=$(dirname "$r1")
-echo Copy and paste the following to bashrc:
-echo 
-echo "export MATLABPATH=${r1}/UFDEV/Project/matlab/latex;$r1/UFDEV/Project/matlab/MPS/common;$r1/UFDEV/Project/matlab/MPS;$r1/UFDEV/Project/matlab/MPS/main;$r1/UFDEV/Project/matlab/MPS/main_rods"
 echo
+echo $r1
+echo
+while true; do
+    read -p "Is this the correct parent directory?(y,n) " yn
+    case $yn in
+        [Yy]* ) echo "Copy this path and paste to the end of your ~/.bashrc file";echo; echo "export MATLABPATH=${r1}/UFDEV/Project/matlab/latex;$r1/UFDEV/Project/matlab/MPS/common;$r1/UFDEV/Project/matlab/MPS;$r1/UFDEV/Project/matlab/MPS/main;$r1/UFDEV/Project/matlab/MPS/main_rods";echo; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
