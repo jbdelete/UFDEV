89ff4510f95c29d4d91f65e137814651787f348c
Jack <jb2ndr@ufl.edu>
2024-09-06 19:52:26 -0400

    added files

---
+++ Project/matlab/MPS/main_rods/rods001.m
@@ -0,0 +1,32 @@
+ rot = 0;
+ mp.AddRod(  11.0,...    % X position
+                    15.00,...   % Y Position
+                    4.00,...    % Z Position
+                    0.20,...    % Radius
+                    0.00,...    % x vel
+                    0.00,...    % y vel
+                    0.00,...    % z vel
+                    particleDistance,...   %Interpatricle distance
+                    rot,...     % rotation around center particle
+                    1,...       % Rod Number
+                    5,...       % num particles per rod
+                    1.0,...     % sequence Number
+                    0.0,...     % attraction coefficient
+                    0.0,...     % repulsion coefficient
+                    "He");      % Substance  
+rot = pi()/16.0;
+mp.AddRod(  11.0,...    % X position
+                    15.52,...   % Y Position
+                    4.00,...    % Z Position
+                    0.20,...    % Radius
+                    0.00,...    % x vel
+                    -0.05,...    % y vel
+                    0.00,...    % z vel
+                    particleDistance,...   %Interpatricle distance
+                    rot,...     % rotation around center particle
+                    2,...       % Rod Number
+                    5,...       % num particles per rod
+                    1.0,...     % sequence Number
+                    0.0,...     % attraction coefficient
+                    0.0,...     % repulsion coefficient
+                    "He");      % Substance    
